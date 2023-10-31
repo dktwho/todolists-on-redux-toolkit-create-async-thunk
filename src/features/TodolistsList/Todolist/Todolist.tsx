19cab835a634b893d57e4aa89060b50071e1ee46
Alexander Korobov <dktwho@mail.ru>
2023-10-31 13:33:01 +0300

    modified fetchTasksTC to  tasksThunks.fetchTasks with createAsyncThunk

--- src/features/TodolistsList/Todolist/Todolist.tsx
+++ src/features/TodolistsList/Todolist/Todolist.tsx
@@ -1,13 +1,13 @@
-import React, { useCallback, useEffect } from "react";
-import { AddItemForm } from "../../../components/AddItemForm/AddItemForm";
-import { EditableSpan } from "../../../components/EditableSpan/EditableSpan";
-import { Task } from "./Task/Task";
-import { TaskStatuses, TaskType } from "../../../api/todolists-api";
-import { FilterValuesType, TodolistDomainType } from "features/TodolistsList/todolists.reducer";
-import { fetchTasksTC } from "features/TodolistsList/tasks.reducer";
-import { useAppDispatch } from "../../../hooks/useAppDispatch";
-import { Button, IconButton } from "@mui/material";
-import { Delete } from "@mui/icons-material";
+import React, {useCallback, useEffect} from "react";
+import {Task} from "./Task/Task";
+import {FilterValuesType, TodolistDomainType} from "features/TodolistsList/todolists.reducer";
+import {Button, IconButton} from "@mui/material";
+import {Delete} from "@mui/icons-material";
+import {EditableSpan} from "components/EditableSpan/EditableSpan";
+import {AddItemForm} from "components/AddItemForm/AddItemForm";
+import {TaskStatuses, TaskType} from "api/todolists-api";
+import {useAppDispatch} from "../../../hooks/useAppDispatch";
+import { tasksThunks } from "../tasks.reducer";
 
 type PropsType = {
   todolist: TodolistDomainType;
@@ -29,8 +29,7 @@ export const Todolist = React.memo(function ({ demo = false, ...props }: PropsTy
     if (demo) {
       return;
     }
-    const thunk = fetchTasksTC(props.todolist.id);
-    dispatch(thunk);
+    dispatch(tasksThunks.fetchTasks(props.todolist.id))
   }, []);
 
   const addTask = useCallback(
